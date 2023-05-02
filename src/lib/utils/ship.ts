557a062bd5a89105df9ec70eca7999c2231d6415
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-05-02 16:07:40 -0300

    Remove some code

--- src/lib/utils/ship.ts
+++ src/lib/utils/ship.ts
@@ -2,12 +2,9 @@ const ship = (length: number) => {
 	let health = 0;
 	const hit = (): number => (health += 1);
 	const isSunk = (): boolean => {
-		console.log(health, 'h');
-		console.log(length, 'l');
 		if (health == length) {
 			return true;
 		}
-
 		return false;
 	};
 
