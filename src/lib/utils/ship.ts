573a796da671fc9622d2f6d632518495eba11361
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-05-08 14:22:40 -0300

    Remove some code

--- src/lib/utils/ship.ts
+++ src/lib/utils/ship.ts
@@ -1,4 +1,4 @@
-const ship = (length: number, axis = 'x', coorX: number, coorY: number) => {
+const ship = (length: number /* , axis = 'x', coorX: number, coorY: number */) => {
 	let health = 0;
 	const hit = (): number => (health += 1);
 	const isSunk = (): boolean => {
