b040646ceaec5aa4377ea8568e1f2b1a58fb4e10
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-05-08 16:27:06 -0300

    Idk what to do next

--- src/lib/utils/gameBoard.ts
+++ src/lib/utils/gameBoard.ts
@@ -2,15 +2,6 @@ import { ship } from './ship';
 
 const gameBoard = (player: string) => {
 	const id = () => player;
-	/* 	const createArr = (() => {
-		const arr: number[][] = [];
-		for (let i = 0; i < 10; i += 1) {
-			for (let j = 0; j < 10; j += 1) {
-				arr[i][j] = 1;
-			}
-		}
-	})(); */
-
 	const Ships = {
 		patrolBoat: ship(2),
 		submarine: ship(3),
@@ -23,3 +14,11 @@ const gameBoard = (player: string) => {
 };
 
 export { gameBoard };
+/* 	const createArr = (() => {
+		const arr: number[][] = [];
+		for (let i = 0; i < 10; i += 1) {
+			for (let j = 0; j < 10; j += 1) {
+				arr[i][j] = 1;
+			}
+		}
+	})(); */
