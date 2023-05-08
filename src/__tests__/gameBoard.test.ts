e6c4d136409c2e3395129d2fb3bb1b198c87ab42
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-05-08 14:21:27 -0300

    Add test for ships in gameboard

--- src/__tests__/gameBoard.test.ts
+++ src/__tests__/gameBoard.test.ts
@@ -5,7 +5,24 @@ import '@testing-library/jest-dom';
 import { gameBoard } from '$lib/utils/gameBoard';
 
 describe('Gameboard', () => {
-	test('test', () => {
-		expect(gameBoard()).toBeFalsy();
+	const player = gameBoard('Wulfis');
+
+	test('id player', () => {
+		expect(player.id()).toBe('Wulfis');
+	});
+
+	test('carrier is sunk?, hit once', () => {
+		expect(player.Ships.carrier.isSunk()).toBeFalsy();
+	});
+
+	test('submarine is sunk? hit twice', () => {
+		player.Ships.submarine.hit();
+		player.Ships.submarine.hit();
+		expect(player.Ships.submarine.isSunk()).toBeFalsy();
+	});
+
+	test('submarine is sunk? hit three times', () => {
+		player.Ships.submarine.hit();
+		expect(player.Ships.submarine.isSunk()).toBeTruthy();
 	});
 });
