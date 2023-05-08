50c4278dee9dd8d8d82bf139fb60f1c5014b2dd0
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-05-08 14:22:03 -0300

    Add ships and id

--- src/lib/utils/gameBoard.ts
+++ src/lib/utils/gameBoard.ts
@@ -1,3 +1,25 @@
-const gameBoard = () => {};
+import { ship } from './ship';
+
+const gameBoard = (player: string) => {
+	const id = () => player;
+	/* 	const createArr = (() => {
+		const arr: number[][] = [];
+		for (let i = 0; i < 10; i += 1) {
+			for (let j = 0; j < 10; j += 1) {
+				arr[i][j] = 1;
+			}
+		}
+	})(); */
+
+	const Ships = {
+		patrolBoat: ship(2),
+		submarine: ship(3),
+		destroyer: ship(3),
+		battleship: ship(4),
+		carrier: ship(5)
+	};
+
+	return { id, Ships };
+};
 
 export { gameBoard };
